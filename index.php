10c8fa205afc4fecd156b56eac1ea7463e52e1f0
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2024-06-28 15:13:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,37 +2,36 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Entregas</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/media.css">
+    <script type="text/javascript" src="js/script.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script>
+        setTimeout(function() {
+            document.getElementById('mensagem').style.display = 'none';
+        }, 3000);
+    </script>
 </head>
 <body>
-    <header>
-        <nav class="navbar bg-body-tertiary">
-            <div class="container-fluid">
-                <span class="navbar-brand mb-0 h1">Sistema de Entregas</span>
-            </div>
-        </nav>
-    </header>
-    <main class="container text-center mt-5">
-        <h2 class="mb-3">Login</h2>
-        <form action="autenticacao.php" method="post" style="max-width: 300px; margin: auto;">
-            <?php if (isset($_GET['mensagem'])) { ?>
-                <div>
-                    <?= $_GET['mensagem'] ?>
-                </div>
-            <?php } ?>
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" name="nome" id="nome" required>
-                <label for="nome">Nome</label>
-            </div>
-            <div class="form-floating mb-3">
-                <input type="password" class="form-control" name="senha" id="senha" required>
-                <label for="senha">Senha</label>
-            </div>
-            <button class="btn btn-primary" type="submit" name="entrar">Entrar</button>
-        </form>
-    </main>
+    <div id="container">
+        <div class="box-login">
+            <h1>Sistemas de Entregas</h1>
+            <form action="autenticacao.php" method="post" class="box">
+                <h2>Faça o seu login</h2>
+                <?php if (isset($_GET['mensagem'])) { ?>
+                    <div id="mensagem" style="width:100%; height:25px; color:black; background-color:#f8d7da; text-align:center; padding-top:10px; border:1px solid red; border-radius:6px;">
+                        <?= $_GET['mensagem'] ?>
+                    </div>
+                <?php } ?>
+                <input type="text" class="form-control" name="nome" id="nome" placeholder="Nome" required>
+                <input type="password" class="form-control" name="senha" id="senha" placeholder="Senha" required>
+
+                <button type="submit" name="entrar">Entrar</button>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
