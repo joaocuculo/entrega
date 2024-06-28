9bf34459aa3c2f115e32fafdd292cff3ca2e5abb
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2024-06-28 13:56:05 -0300

    Create menu01.php

---
+++ template/menu01.php
@@ -0,0 +1,31 @@
+<header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">DTI</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="../home/principal.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../home/relatorio.php">Relatório</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Cadastros
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="../cadastro/cad-usuario.php">Usuário</a></li>
+                        <li><a class="dropdown-item" href="../cadastro/cad-tecnico.php">Técnico</a></li>
+                        <li><a class="dropdown-item" href="../cadastro/cad-entrega.php">Entrega</a></li>
+                    </ul>
+                </li>
+            </ul>
+            </div>
+            <a class="navbar-brand" href="../sair.php">Sair</a>
+        </div>
+    </nav>
+</header>
