4d0d7c5b80694aa6face4ebfcf643c47e44c9152
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2024-07-05 11:03:15 -0300

    Update rodape01.php

--- template/rodape01.php
+++ template/rodape01.php
@@ -1,6 +1,7 @@
 <!-- Sticky Footer -->
-<footer id="sticky-footer" class="py-4 bg-dark text-white-50">
-    <div class="container text-center">
-        <small>Copyright &copy; DTI 2024</small>
-    </div>
-</footer>
+<footer id="sticky-footer" style="background: linear-gradient(to right, #000000, #000646); color: white;">
+        <div class="container text-center">
+            <small>Copyright &copy; DTI 2024</small>
+        </div>
+    </footer>
+
