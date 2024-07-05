68ff9bcf4bfb85c986f67ae21c6f4ea2413f7959
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2024-07-05 11:26:21 -0300

    Add files via upload

--- list/list-usuario.php
+++ list/list-usuario.php
@@ -41,6 +41,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listagem de Usuários</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <style>
         body {
@@ -80,8 +81,9 @@
         <table class="table table-striped table-bordered">
             <thead>
                 <tr>
-                    <th>Usuário</th>
-                    <th>Status</th>
+                    <th class="col-6">Usuário</th>
+                    <th class="col-4">Status</th>
+                    <th class="d-flex justify-content-center">Ações</th>
                 </tr>
             </thead>
             <tbody>
@@ -96,6 +98,10 @@
                 <tr>
                     <td><?= $linha['nome'] ?></td>
                     <td><?= $status ?></td>
+                    <td class="d-flex justify-content-center gap-2">
+                        <abbr title="Editar"><a class="btn btn-warning" href="../editar/edit-usuario.php?id=<?= $linha['id'] ?>"><i class="bi bi-pencil-fill"></i></a></abbr>
+                        <abbr title="Desativar"><a class="btn btn-danger"><i class="bi bi-person-dash-fill"></i></a></abbr>
+                    </td>
                 </tr>
                 <?php } ?>
             </tbody>
