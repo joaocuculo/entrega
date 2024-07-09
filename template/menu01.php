d496c0b6f5d18826092c79c1b08ef065872ef4bc
andresantos <zezinp2p3@gmail.com>
2024-07-09 10:52:15 -0300

    Update menu01.php

--- template/menu01.php
+++ template/menu01.php
@@ -1,41 +1,35 @@
-<header>
-    <nav class="navbar bg-dark border-bottom navbar-expand-lg border-body" data-bs-theme="dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="../home/relatorio.php">DTI</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+<nav class="navbar navbar-expand-lg" style="background: linear-gradient(to right, #000000, #000646); border-bottom: 1px solid #292E36;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="../home/relatorio.php" style="color: white;">DTI</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="../home/relatorio.php">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../home/perfil.php">Perfil</a>
+                    <a class="nav-link" href="../home/perfil.php" style="color: white;">Perfil</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white;">
                         Cadastros
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="../cadastro/cad-usuario.php">Usuário</a></li>
-                        <li><a class="dropdown-item" href="../cadastro/cad-tecnico.php">Técnico</a></li>
-                        <li><a class="dropdown-item" href="../cadastro/cad-entrega.php">Entrega</a></li>
+                        <li><a class="dropdown-item" href="../cadastro/cad-usuario.php" style="color: black;">Usuário</a></li>
+                        <li><a class="dropdown-item" href="../cadastro/cad-tecnico.php" style="color: black;">Técnico</a></li>
+                        <li><a class="dropdown-item" href="../cadastro/cad-entrega.php" style="color: black;">Entrega</a></li>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white;">
                         Listagem
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="../listar/list-usuario.php">Usuário</a></li>
-                        <li><a class="dropdown-item" href="../listar/list-tecnico.php">Técnico</a></li>
+                        <li><a class="dropdown-item" href="../listar/list-usuario.php" style="color: black;">Usuário</a></li>
+                        <li><a class="dropdown-item" href="../listar/list-tecnico.php" style="color: black;">Técnico</a></li>
                     </ul>
                 </li>
             </ul>
-            </div>
-            <a class="navbar-brand" href="../sair.php">Sair</a>
         </div>
-    </nav>
+        <a class="navbar-brand" href="../sair.php" style="color: white;">Sair</a>
+    </div>
 </nav>
-</header>
