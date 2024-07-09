ef3793b00c1753bf554c03710d4ad4ce281ae203
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2024-07-09 10:06:59 -0300

    Add files via upload

--- entrega.sql
+++ entrega.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 28/06/2024 às 18:45
+-- Tempo de geração: 09/07/2024 às 15:06
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -43,11 +43,6 @@ CREATE TABLE `tabela` (
 --
 
 INSERT INTO `tabela` (`id`, `id_usuario`, `data`, `chamado`, `id_tecnico`, `recebedor`) VALUES
-(7, 1, '2024-02-22', '021254', 2, 'Marcia'),
-(8, 1, '2024-04-15', '021254', 1, 'Maurício'),
-(9, 2, '2023-05-04', '023254', 3, 'Marcelo'),
-(10, 2, '2024-06-21', '022546', 4, 'Sandra'),
-(11, 2, '0000-00-00', '022222', 4, 'Daniel'),
 (12, 2, '25/12/2023', '022222', 4, 'Daniel'),
 (13, 2, '28/10/2022', '020232', 3, 'Ricardo'),
 (14, 2, '12/06/2024', '021235', 3, 'Pedro'),
@@ -59,7 +54,12 @@ INSERT INTO `tabela` (`id`, `id_usuario`, `data`, `chamado`, `id_tecnico`, `rece
 (20, 1, '08/09/2023', '020147', 6, 'Tom'),
 (21, 1, '08/09/2023', '020147', 6, 'Tom'),
 (22, 1, '04/05/2024', '020004', 6, 'Robben'),
-(23, 2, '08/05/2023', '020369', 5, 'Silva');
+(23, 2, '10/05/2023', '020369', 5, 'Silva'),
+(24, 2, '12/02/2024', '023258', 1, 'Renato'),
+(25, 1, '01/07/2024', '024212', 1, 'Cleiton'),
+(26, 1, '05/05/2024', '024267', 4, 'Douglas'),
+(27, 1, '11/09/2023', '022178', 7, 'Yago'),
+(28, 1, '07/07/2024', '020369', 1, 'Fabrício');
 
 -- --------------------------------------------------------
 
@@ -83,7 +83,8 @@ INSERT INTO `tecnico` (`id`, `nome`, `status`) VALUES
 (3, 'Eduardo', 1),
 (4, 'João', 1),
 (5, 'Eduardo M.', 1),
-(6, 'Marcos', 1);
+(6, 'Marcos', 1),
+(7, 'Otávio', 2);
 
 -- --------------------------------------------------------
 
@@ -104,9 +105,11 @@ CREATE TABLE `usuario` (
 
 INSERT INTO `usuario` (`id`, `nome`, `senha`, `status`) VALUES
 (1, 'Joao', '1234', 1),
-(2, 'Andre', '4321', 1),
+(2, 'André', '4321', 1),
 (3, 'Eduardo', '2345', 1),
-(4, 'Marcos', '9876', 1);
+(4, 'Marcos', '9876', 1),
+(5, 'José', 'jose', 2),
+(6, 'Cesar', 'soucesar', 2);
 
 --
 -- Índices para tabelas despejadas
@@ -140,19 +143,19 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `tabela`
 --
 ALTER TABLE `tabela`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- AUTO_INCREMENT de tabela `tecnico`
 --
 ALTER TABLE `tecnico`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Restrições para tabelas despejadas
