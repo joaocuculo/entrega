50b3af4cbfff0b3dad03f1dacd64a4d580630757
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2024-07-10 16:42:00 -0300

    Update menu01.php

--- template/menu01.php
+++ template/menu01.php
@@ -1,35 +1,47 @@
-<nav class="navbar navbar-expand-lg" style="background: linear-gradient(to right, #000000, #000646); border-bottom: 1px solid #292E36;">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="../home/relatorio.php" style="color: white;">DTI</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
+<header>
+    <nav class="navbar navbar-expand-lg" style="background: linear-gradient(to right, #000000, #000646); border-bottom: 1px solid #292E36;" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../home/relatorio.php">DTI</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link" href="../home/perfil.php" style="color: white;">Perfil</a>
+                    <a class="nav-link" href="../home/relatorio.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../home/perfil.php">Perfil</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white;">
+                    <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Cadastros
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="../cadastro/cad-usuario.php" style="color: black;">Usuário</a></li>
-                        <li><a class="dropdown-item" href="../cadastro/cad-tecnico.php" style="color: black;">Técnico</a></li>
-                        <li><a class="dropdown-item" href="../cadastro/cad-entrega.php" style="color: black;">Entrega</a></li>
+                        <li><a class="dropdown-item" href="../cadastro/cad-usuario.php">Usuário</a></li>
+                        <li><a class="dropdown-item" href="../cadastro/cad-tecnico.php">Técnico</a></li>
+                        <li><a class="dropdown-item" href="../cadastro/cad-entrega.php">Entrega</a></li>
+                        <?php if ($_SESSION['nivel'] == 2): ?>
+                            <li><a class="dropdown-item" href="../cadastro/cad-adm.php">Administrador</a></li>
+                        <?php endif; ?>
                     </ul>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white;">
+                    <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Listagem
                     </a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="../listar/list-usuario.php" style="color: black;">Usuário</a></li>
-                        <li><a class="dropdown-item" href="../listar/list-tecnico.php" style="color: black;">Técnico</a></li>
+                        <li><a class="dropdown-item" href="../listar/list-usuario.php">Usuário</a></li>
+                        <li><a class="dropdown-item" href="../listar/list-tecnico.php">Técnico</a></li>
+                        <?php if ($_SESSION['nivel'] == 2): ?>
+                            <li><a class="dropdown-item" href="../listar/list-adm.php">Administrador</a></li>
+                        <?php endif; ?>
                     </ul>
                 </li>
             </ul>
+            </div>
+            <a class="navbar-brand" href="../sair.php">Sair</a>
         </div>
-        <a class="navbar-brand" href="../sair.php" style="color: white;">Sair</a>
-    </div>
+    </nav>
 </nav>
+</header>
