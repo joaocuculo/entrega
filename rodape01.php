1ffafbff03ccfe7f1bf3d8187d2b2019d5850c6f
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2024-07-15 09:51:49 -0300

    Delete rodape01.php

--- rodape01.php
+++
@@ -1,6 +0,0 @@
-<!-- Sticky Footer -->
-<footer id="sticky-footer" style="background: linear-gradient(to right, #000307, #00070E); color: white;">
-    <div class="container text-center">
-        <small>Copyright &copy; DTI 2024</small>
-    </div>
-</footer>
