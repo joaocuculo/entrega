32fe6962e91285427b9175b5fc5dc5ff72167ecb
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2024-07-15 09:57:32 -0300

    Update perfil.php

--- home/perfil.php
+++ home/perfil.php
@@ -127,7 +127,7 @@
             <div class="col-md-6">
                 <form method="post">
                     <?php if (!empty($mensagem)) { ?>
-                        <div id="mensagem" class="alert alert-success mb-3">
+                        <div id="mensagem" class="alert alert-success mb-3" style="background-color:#051B11; color:white;">
                             <?= $mensagem ?>
                         </div>
                     <?php } ?>  
