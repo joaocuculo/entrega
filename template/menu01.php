b90b7ba931a29bb20ca77d72d07116993a53536b
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2024-09-06 10:54:55 -0300

    Add files via upload

--- template/menu01.php
+++ template/menu01.php
@@ -3,27 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Toggle de Mudança de Cor do Site</title>
-    <style>
-        /* Estilo padrão (tema escuro) */
-        body {
-            background: linear-gradient(to right, #000307, #000B18);
-            color: #fff; /* Cor do texto para o tema escuro */
-        }
-
-        /* Tema claro */
-        .theme-light {
-            background: linear-gradient(to right, #ffffff, #ffffff);
-            color: #333; /* Cor do texto para o tema claro */
-        
-        }
-    </style>
+   
 </head>
 
     <header>
         <nav class="navbar fixed-top navbar-expand-lg" style="background: linear-gradient(to right, #000307, #00070E);" data-bs-theme="dark">
             <div class="container-fluid">
+                
                 <!-- Logo e Links -->
+                
                 <a class="navbar-brand" href="../home/relatorio.php">ＤＴＩ</a>
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
@@ -38,7 +26,9 @@
                         </a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="../cadastro/cad-entrega.php">Entrega</a></li>
+                            
                             <!-- Mostrar apenas se o usuário tiver o nível adequado -->
+                            
                             <?php if ($_SESSION['nivel'] == 2): ?>
                                 <li><a class="dropdown-item" href="../cadastro/cad-usuario.php">Usuário</a></li>
                                 <li><a class="dropdown-item" href="../cadastro/cad-tecnico.php">Técnico</a></li>
@@ -61,13 +51,10 @@
                     </li>
                     <?php endif; ?>
                 </ul>
-                 <div>
-                     <a id="toggleButton"  style="cursor:pointer;margin-right: 1rem;">
-                         <i class="bi bi-circle-half"></i> <!-- botao de alterar tema -->
-                            </a>
-                        
+                    <div>        
                         
                         <!-- botão Sair -->
+                        
                         <a class="navbar-brand ms-auto" href="../sair.php">Sair <i class="bi bi-box-arrow-in-left"></i></a>
                     </div>               
             </div>
