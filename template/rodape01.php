9d6c80ed4260d23cd5888aee98d4fa68c1342785
João Victor Codato Cuculo <123258227+joaocuculo@users.noreply.github.com>
2024-09-20 11:17:57 -0300

    Add files via upload

--- template/rodape01.php
+++ template/rodape01.php
@@ -1,6 +1,6 @@
 <!-- Sticky Footer -->
 <footer id="sticky-footer" style="background: linear-gradient(to right, #000307, #00070E); color: white;">
     <div class="container text-center">
-        <small>Copyright &copy; DTI 2024</small>
+        <small>Copyright &copy; DTI 2024 João & André</small>
     </div>
 </footer>
